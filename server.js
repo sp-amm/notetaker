40df8d09472843ce4bd4a48d2869f079b905bf7a
sp <sam.pack.sp@gmail.com>
2020-11-07 17:59:17 +1030

    html routes working

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ app.get("/", (req,res)=> {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("./public/index.html", (req,res)=> {
+app.get("/index.html", (req,res)=> {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("./public/notes.html", (req,res)=> {
+app.get("/notes.html", (req,res)=> {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
 
