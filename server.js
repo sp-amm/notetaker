c4d052960dc70611d6edc6ca033dfda7b813b933
sp <sam.pack.sp@gmail.com>
2020-11-07 20:49:52 +1030

    added id to new note

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const notes = require("./db.json");
 const express = require("express");
+const { networkInterfaces } = require("os");
 const app = express();
 
 
@@ -28,15 +29,27 @@ app.get("/api/notes", function(req, res){
 });
 
 //API Post request to handle the user submitted data.
+//* POST `/api/notes` - Should receive a new note to save on the request body, 
+//add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", function(req, res){
     //console.log(req.body);
-    notes.push(req.body);
+    id = notes.length + 1;
+    newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: id,
+    }
+    notes.push(newNote);
     console.log(notes);
     //JSON.parse(notes)
-    res.json(true);
+    res.json(notes);
 })
 
 //API delete request to delete when requested.
+//* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note
+// to delete. This means you'll need to find a way to give each note a unique `id` when 
+//it's saved. In order to delete a note, you'll need to read all notes from the `db.json` 
+//file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 app.delete("api/notes/:id", function(req, res){
 
 });
